a31d323a7f4f27a9fabe6cfb55ab65afd1e65193
AndriiSolodaiev <andik05016@gmail.com>
2023-12-04 18:00:29 +0200

    change header

--- src/assets/scripts/gulp-modules/location.js
+++ src/assets/scripts/gulp-modules/location.js
@@ -2,7 +2,7 @@ import 'swiper/css';
 import { gsap, ScrollTrigger } from 'gsap/all';
 import { initSmoothScrolling } from '../modules/scroll/leniscroll';
 import googleMap from '../modules/map/map';
-
+import '../modules/helpers/imgParallax';
 googleMap();
 initSmoothScrolling();
 gsap.registerPlugin(ScrollTrigger);
